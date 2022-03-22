22bc8cc50a28050c32172cd05a23dc5cf5c1ae0d
Kale Leach <kleach1212@gmail.com>
2022-03-21 17:29:27 -0700

    commit tree 7fb4de7a6bf5dd3d76a956445394489def8b3f35

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  //console.log(links)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksArr = links.map((link) => <a key = {link} href = {"#" + link} > {link} </a>)
+  //console.log(linksArr)
+  return <nav>{linksArr}</nav>;
 }
 
 export default NavBar;
